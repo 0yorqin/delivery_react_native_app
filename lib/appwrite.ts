3514e21c5ebd47a21232d13cf1e5929e2f4e783c
Yorqin Rahimboboyev <crossrick88@gmail.com>
2025-08-05 21:36:52 +0500

    Auth UI

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwiteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.mixburger.deliveryapp",
   projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "6891acb20022f7648a0c",
+  userCollectionId: "6891accd0023ae215f42",
 };

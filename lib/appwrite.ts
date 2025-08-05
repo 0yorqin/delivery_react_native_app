8c9f3a88bb590696a584f821083139a3410e77e3
Yorqin Rahimboboyev <crossrick88@gmail.com>
2025-08-05 12:01:27 +0500

    Connect Appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwiteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.mixburger.deliveryapp",
+  projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
